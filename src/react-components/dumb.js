11d415c961cfc9d7684da1e62f025e333914e1cd
peterhoward42 <peterhoward42@gmail.com>
2017-01-29 00:08:09 +0000

    Wip

--- src/react-components/dumb.js
+++ src/react-components/dumb.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+
+// These are written bottom up, w.r.t. the component hierarchy.
 
 
-/** An element that displays the content of an email. */
+/** An element that displays the text contents of an email. */
 export function ContentsOfEmail(props) {
-  return (<div>{props.contentsText}</div>);
+    return (<div>{props.contentsText}</div>)
+}
+
+/** An element that displays a fixed format header for an email,
+ * parameterised with an email number.
+ **/
+export function HeaderOfEmail(props) {
+    return (<h3>Email #{props.emailNumber}</h3>)
+}
+
+/** An element that displays the given rows of text, inside a
+ * Bootstrap ListGroup.
+ **/
+export function ListOfEmails(props) {
+    return (
+        <ListGroup>
+            <ListGroupItem>Item 1</ListGroupItem>
+            <ListGroupItem>Item 2</ListGroupItem>
+            <ListGroupItem>Item 3</ListGroupItem>
+        </ListGroup>)
 }
--- src/react-components/dumb.test.js
+++ src/react-components/dumb.test.js
@@ -1,10 +1,33 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, render } from 'enzyme'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+
 import * as dumb from './dumb.js'
 
-it('foo test', () => {
-    const rendered = shallow(<dumb.ContentsOfEmail contentsText='XXX' />)
-    expect(rendered.type()).toEqual('div')
-    expect(rendered.text()).toEqual('XXX')
+
+// These are written bottom up, w.r.t. the component hierarchy.
+
+it('ContentsOfEmail renders given text in div.', () => {
+    const wrapper = shallow(<dumb.ContentsOfEmail contentsText='XXX' />)
+    expect(wrapper.type()).toEqual('div')
+    expect(wrapper.text()).toEqual('XXX')
 })
+
+it('EmailHeader renders given text as h3.', () => {
+    const wrapper = shallow(<dumb.HeaderOfEmail emailNumber={3} />)
+    expect(wrapper.type()).toEqual('h3')
+    expect(wrapper.text()).toEqual('Email #3')
+})
+
+it('ListOfEmails renders a Bootstrap ListGroup with injected content.', () => {
+    const wrapper = shallow(<dumb.ListOfEmails />)
+    expect(wrapper.type()).toEqual(ListGroup)
+    const children = wrapper.children()
+    expect(children.length).toEqual(3)
+    const child = wrapper.childAt(1)
+    expect(child.type()).toEqual(ListGroupItem)
+    const listItemText = child.children()
+    expect(listItemText.text()).toEqual('Item 2')
+})
+
 
